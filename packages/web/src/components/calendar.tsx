19ca48f84fcf534eeaabfc38dffc3aa9db064b3b
zepez <zepezaueralex@gmail.com>
2024-05-04 17:47:02 -0400

    💄 Responsive iframe calendar embed

--- packages/web/src/components/calendar.tsx
+++ packages/web/src/components/calendar.tsx
@@ -13,13 +13,16 @@ export const Calendar = () => {
         </div>
         <p className="text-right text-[6rem] leading-none">🎯</p>
       </div>
-      <div className="mx-auto rounded-lg bg-white p-2 opacity-90">
-        <iframe
-          src="https://calendar.google.com/calendar/embed?src=aeb4b83ed30e8807539f452581bd83dc7dc914ae0af72dc5419fe10c7a5230e1%40group.calendar.google.com&ctz=America%2FNew_York"
-          style={{ borderWidth: 0 }}
-          width="100%"
-          height="600"
-        />
+      <div className="mx-auto rounded-lg bg-white p-2 lg:p-3">
+        <div className="iframe-container">
+          <iframe
+            src="https://calendar.google.com/calendar/embed?src=aeb4b83ed30e8807539f452581bd83dc7dc914ae0af72dc5419fe10c7a5230e1%40group.calendar.google.com&ctz=America%2FNew_York"
+            style={{ borderWidth: 0 }}
+            width="100%"
+            height="600"
+            className="iframe"
+          />
+        </div>
       </div>
     </Section>
   );
