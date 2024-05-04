fe6a55042622cdc89266f4bc6745f2c5ddaea9bc
zepez <zepezaueralex@gmail.com>
2024-05-04 19:09:21 -0400

    ✨ Start adding articles :)

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -1,8 +1,12 @@
+import createMDX from "@next/mdx";
 import { withPlausibleProxy } from "next-plausible";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+};
 
+const withMDX = createMDX();
 const withPlausible = withPlausibleProxy();
 
-export default withPlausible(nextConfig);
+export default withPlausible(withMDX(nextConfig));
